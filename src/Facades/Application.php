53757bc5d42c7c8fc7291d2dca5a9153d19b632c
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-05-26 09:07:14 +0300

    Added facades for user and application

---
+++ src/Facades/Application.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PrettyBx\Support\Facades;
+
+use PrettyBx\Support\Base\AbstractFacade;
+
+class Application extends AbstractFacade
+{
+    /**
+     * @inheritDoc
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'CMain';
+    }
+}
